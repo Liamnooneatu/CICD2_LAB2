3e70ecbdaaf719845098a82b86ea16db4fbcce62
Liam <G00420041@atu.ie>
2025-10-03 11:40:09 +0100

    Lab 2: First tests, coverage and CI Tests with pytest

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+# app/main.py
 from fastapi import FastAPI, HTTPException, status
 from .schemas import User
 
@@ -36,8 +37,6 @@ def update_user(user_id: int, updated_user: User):
             return updated_user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
-
-# DELETE: remove user
 @app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user_id: int):
     for index, u in enumerate(users):
@@ -46,8 +45,6 @@ def delete_user(user_id: int):
             return
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
-
-# HEALTH CHECK
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
